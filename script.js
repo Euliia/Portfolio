b1a647865ee196ec06bc84d85fcb85009bf770af
LAPTOP-PHSLA1FS\julia <juliagunnlaugs05@gmail.com>
2026-01-28 08:24:38 +0000

    minor chnages to colour

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 const canvas = document.getElementById("starsCanvas");
 const ctx = canvas.getContext("2d");
 
-let stars = [];
 let w = 0;
 let h = 0;
+let stars = [];
 
 function resizeCanvas() {
     w = canvas.width = window.innerWidth;
     h = canvas.height = window.innerHeight;
 
-    const count = Math.floor((w * h) / 14000);
     stars = [];
+    const count = Math.floor((w * h) / 20000);
 
     for (let i = 0; i < count; i++) {
         stars.push({
             x: Math.random() * w,
             y: Math.random() * h,
-            r: Math.random() * 1.6 + 0.2,
-            a: Math.random() * 0.6 + 0.2,
-            tw: Math.random() * 0.015 + 0.005
+            r: Math.random() * 1.2 + 0.2,
+            a: Math.random() * 0.35 + 0.08,
+            drift: Math.random() * 0.25 + 0.05
         });
     }
 }
@@ -29,13 +29,12 @@ function drawStars() {
     for (let i = 0; i < stars.length; i++) {
         const s = stars[i];
 
-        s.a += s.tw * (Math.random() > 0.5 ? 1 : -1);
-        if (s.a < 0.15) s.a = 0.15;
-        if (s.a > 0.9) s.a = 0.9;
+        s.y += s.drift;
+        if (s.y > h) s.y = 0;
 
         ctx.beginPath();
         ctx.arc(s.x, s.y, s.r, 0, Math.PI * 2);
-        ctx.fillStyle = "rgba(255,255,255," + s.a + ")";
+        ctx.fillStyle = "rgba(35, 40, 55, " + s.a + ")";
         ctx.fill();
     }
 
@@ -43,6 +42,14 @@ function drawStars() {
 }
 
 window.addEventListener("resize", resizeCanvas);
+
+function githubRedirect() {
+    globalThis.location.href = "https://github.com/Euliia";
+}
+
+function linkedinRedirect() {
+    globalThis.location.href = "https://www.linkedin.com/in/júlía-gunnlaugsdóttir";
+}
 
 resizeCanvas();
 drawStars();
